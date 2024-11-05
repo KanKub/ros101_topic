d44c300ccab38725ab8a6c0fe35ff32d1c52bfd2
KanKub <gun101010@gmail.com>
2024-11-06 06:49:02 +0700

    Init commit

---
+++ Exam1_Topic/ros2_test_ws/src/my_package/my_package/my_node_subscriber.py
@@ -0,0 +1,34 @@
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class SimpleSubscriberNode(Node):
+    def __init__(self):
+        super().__init__('simple_subscriber_node')
+        # Create a subscription to the topic 'topic_name'
+        self.subscription = self.create_subscription(
+            String,  # Message type
+            'simple_topic',  # Topic name
+            self.listener_callback,  # Callback function
+            1  # Queue size
+        )
+
+    def listener_callback(self, msg):
+        # This callback is called whenever a message is received
+        self.get_logger().info(f'Received: "{msg.data}"')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = SimpleSubscriberNode()
+
+    try:
+        rclpy.spin(node)  # Keeps the node running to listen for messages
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
