69b966316a69ef6c3728f83457ab0a280c2262f7
KanKub <gun101010@gmail.com>
2024-11-06 07:29:52 +0700

    Fix errors

--- Exam1_Topic/ros2_test_ws/src/my_package/my_package/my_node_publisher.py
+++ Exam1_Topic/ros2_test_ws/src/my_package/my_package/my_node_publisher.py
@@ -2,30 +2,37 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 
+
 class SimplePublisherNode(Node):
     def __init__(self):
-        super().__init__('simple_publisher_node')
-        self.publisher_ = self.create_publisher(String, 'simple_topic', 1)  # Topic name and queue size
-        self.timer = self.create_timer(1.0, self.timer_callback)  # Timer callback every 1 second
+        super().__init__("simple_publisher_node")
+        self.publisher_ = self.create_publisher(
+            String, "simple_topic", 1
+        )  # Topic name and queue size
+        self.timer = self.create_timer(
+            1.0, self.timer_callback
+        )  # Timer callback every 1 second
 
     def timer_callback(self):
         msg = String()
-        msg.data = 'Hello, ROS 2!'
+        msg.data = "Hello, ROS 2!"
         self.publisher_.publish(msg)
-        self.get_logger().info(f'Publishing: {msg.data}')
+        self.get_logger().info(f"Publishing: {msg.data}")
+
 
 def main(args=None):
     rclpy.init(args=args)
     node = SimplePublisherNode()
 
     try:
-        rclpy.spin(node)
+        rclpy.spin(node)  # Keeps the node running to listen for messages
     except KeyboardInterrupt:
         pass
-    finally:
-        print("Shutting down.")
-        node.destroy_node()
-        rclpy.shutdown()
 
-if __name__ == '__main__':
+    print("Shutdown.")
+    node.destroy_node()
+    # rclpy.shutdown()
+
+
+if __name__ == "__main__":
     main()
--- Exam1_Topic/ros2_test_ws/src/my_package/my_package/my_node_subscriber.py
+++ Exam1_Topic/ros2_test_ws/src/my_package/my_package/my_node_subscriber.py
@@ -1,22 +1,23 @@
-
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 
+
 class SimpleSubscriberNode(Node):
     def __init__(self):
-        super().__init__('simple_subscriber_node')
+        super().__init__("simple_subscriber_node")
         # Create a subscription to the topic 'topic_name'
         self.subscription = self.create_subscription(
             String,  # Message type
-            'simple_topic',  # Topic name
+            "simple_topic",  # Topic name
             self.listener_callback,  # Callback function
-            1  # Queue size
+            1,  # Queue size
         )
 
     def listener_callback(self, msg):
         # This callback is called whenever a message is received
         self.get_logger().info(f'Received: "{msg.data}"')
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -26,9 +27,11 @@ def main(args=None):
         rclpy.spin(node)  # Keeps the node running to listen for messages
     except KeyboardInterrupt:
         pass
-    finally:
-        node.destroy_node()
-        rclpy.shutdown()
 
-if __name__ == '__main__':
+    print("Shutdown.")
+    node.destroy_node()
+    # rclpy.shutdown()
+
+
+if __name__ == "__main__":
     main()
